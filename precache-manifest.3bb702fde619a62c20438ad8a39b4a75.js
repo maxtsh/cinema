52f55b83d3395b96b3184a1663090eea699c7a81
Max Tsh <tatshahdoost@gmail.com>
2019-11-27 03:26:55 +0330

    Updates

---
+++ precache-manifest.3bb702fde619a62c20438ad8a39b4a75.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79dd72bebf2f1c1435a85be94444284d",
+    "url": "/maxtsh/react-cinema-app/index.html"
+  },
+  {
+    "revision": "1b6e1072db98b0951a51",
+    "url": "/maxtsh/react-cinema-app/static/css/2.3c2621cf.chunk.css"
+  },
+  {
+    "revision": "ec8b0e8b9c0f3508c6eb",
+    "url": "/maxtsh/react-cinema-app/static/css/main.4d300875.chunk.css"
+  },
+  {
+    "revision": "1b6e1072db98b0951a51",
+    "url": "/maxtsh/react-cinema-app/static/js/2.88d529c3.chunk.js"
+  },
+  {
+    "revision": "ec8b0e8b9c0f3508c6eb",
+    "url": "/maxtsh/react-cinema-app/static/js/main.d3aeb84e.chunk.js"
+  },
+  {
+    "revision": "1d21f3193d8262dd5a6b",
+    "url": "/maxtsh/react-cinema-app/static/js/runtime-main.b93f4503.js"
+  },
+  {
+    "revision": "2d8723a54ee4f7064d6a16baa731d88a",
+    "url": "/maxtsh/react-cinema-app/static/media/background1.2d8723a5.jpg"
+  },
+  {
+    "revision": "750425ce9f7abfda3936e90912824a49",
+    "url": "/maxtsh/react-cinema-app/static/media/background2.750425ce.jpg"
+  },
+  {
+    "revision": "8596f53e1cdd57d8994d9a08d1d166d7",
+    "url": "/maxtsh/react-cinema-app/static/media/background3.8596f53e.jpg"
+  },
+  {
+    "revision": "96ce69a6546c32bfe8ed428fa13f49ef",
+    "url": "/maxtsh/react-cinema-app/static/media/background4.96ce69a6.jpg"
+  },
+  {
+    "revision": "33737559095865b5c8e51ada27ee82d7",
+    "url": "/maxtsh/react-cinema-app/static/media/background5.33737559.jpg"
+  },
+  {
+    "revision": "1ead917cfa0925ad87f16dfa4dc7260c",
+    "url": "/maxtsh/react-cinema-app/static/media/background6.1ead917c.jpg"
+  },
+  {
+    "revision": "43d1e24dd6bd86d743283b7859ff5dc9",
+    "url": "/maxtsh/react-cinema-app/static/media/background7.43d1e24d.jpg"
+  },
+  {
+    "revision": "cffa9d9d88890cfcd0ae3d6dd7a6efa9",
+    "url": "/maxtsh/react-cinema-app/static/media/imageLoading.cffa9d9d.gif"
+  },
+  {
+    "revision": "0d0345f662a757ef17eef497fa8e83fc",
+    "url": "/maxtsh/react-cinema-app/static/media/loading.0d0345f6.gif"
+  },
+  {
+    "revision": "ddd6941e65267c452894b27f74f5ea4d",
+    "url": "/maxtsh/react-cinema-app/static/media/person_background.ddd6941e.jpg"
+  }
+]);
