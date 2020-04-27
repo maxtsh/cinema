77fb5a2794f82e8b935a4206f0b00028e4cddc78
Max Tsh <tatshahdoost@gmail.com>
2020-04-27 18:12:08 +0430

    Updates

---
+++ precache-manifest.c29104d59a9a3150a3acb94bbaea0db8.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ba0a70f6d047b6d66a8ebdc06ac6a67",
+    "url": "/react-cinema-app/index.html"
+  },
+  {
+    "revision": "3401e6a45f9fc5c33b2f",
+    "url": "/react-cinema-app/static/css/2.f6a58e12.chunk.css"
+  },
+  {
+    "revision": "c71ef02aecb1a45aee61",
+    "url": "/react-cinema-app/static/css/main.2ce5098b.chunk.css"
+  },
+  {
+    "revision": "3401e6a45f9fc5c33b2f",
+    "url": "/react-cinema-app/static/js/2.8ce0e543.chunk.js"
+  },
+  {
+    "revision": "a5da5d53dfba093fa63375799ffeddd6",
+    "url": "/react-cinema-app/static/js/2.8ce0e543.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c71ef02aecb1a45aee61",
+    "url": "/react-cinema-app/static/js/main.63b7ba55.chunk.js"
+  },
+  {
+    "revision": "d3c4beb9b86c42bd9058",
+    "url": "/react-cinema-app/static/js/runtime-main.56387ee0.js"
+  },
+  {
+    "revision": "f26323d49369581fbf7c99357ba5abdb",
+    "url": "/react-cinema-app/static/media/avatar.f26323d4.png"
+  },
+  {
+    "revision": "2b02159fee58d573c079ad5212d56b63",
+    "url": "/react-cinema-app/static/media/loader.2b02159f.gif"
+  },
+  {
+    "revision": "8c3d2a79d2974762d90310ec87bec6e6",
+    "url": "/react-cinema-app/static/media/notfound.8c3d2a79.svg"
+  },
+  {
+    "revision": "daac33c808b9446d0ed81a47999133ea",
+    "url": "/react-cinema-app/static/media/person_background.daac33c8.jpg"
+  },
+  {
+    "revision": "dd6410360f48685fa2bee80fa789b1f9",
+    "url": "/react-cinema-app/static/media/poster_placeholder.dd641036.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.580b835f55c7bcf0a294a53a31d565e9.js"
+  "/react-cinema-app/precache-manifest.c29104d59a9a3150a3acb94bbaea0db8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-cinema-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
